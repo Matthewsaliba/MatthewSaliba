4bb8e490af900d7ca448edd4a24ef2b4906a9501
Matthewsaliba <Salibamatt05@gmail.com>
2025-09-23 23:52:53 +1000

    fixing pipeline cont

--- app.py
+++
@@ -1,8 +0,0 @@
-import aws_cdk as cdk
-from week2work.pipeline_stack import PipelineStack
-
-app = cdk.App()
-
-PipelineStack(app, "MultiStagePipelineStack", env=cdk.Environment(region="ap-southeast-2"))
-
-app.synth()
