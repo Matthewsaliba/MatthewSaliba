b42f57347886ffdee0865166147b0d76f66655ce
Matthewsaliba <Salibamatt05@gmail.com>
2025-10-07 17:55:19 +1100

    fixed tests

--- lambda_22128867/canary/test_canary_integration.py
+++ lambda_22128867/canary/test_canary_integration.py
@@ -1,8 +1,21 @@
 import os
+import sys
+import types
 import boto3
 import pytest
-from moto import mock_aws
+from moto import mock_sns
 from unittest.mock import patch
+
+if sys.platform == "win32":
+    fake_resource = types.SimpleNamespace()
+    fake_resource.RUSAGE_SELF = 0
+
+    def fake_getrusage(who):
+        return types.SimpleNamespace(ru_maxrss=123456) 
+    fake_resource.getrusage = fake_getrusage
+    sys.modules['resource'] = fake_resource
+
+
 import canary
 
 REGION = "ap-southeast-2"
@@ -19,7 +32,7 @@ def aws_credentials():
 
 @pytest.fixture(scope="function")
 def sns_client(aws_credentials):
-    with mock_aws():
+    with mock_sns():
         client = boto3.client("sns", region_name=REGION)
         yield client
 
@@ -32,11 +45,8 @@ def sns_topic(sns_client):
 
 @pytest.fixture(autouse=True)
 def set_env_vars(sns_topic):
-
-
     os.environ["ALERT_TOPIC_ARN"] = sns_topic
     os.environ["TARGET_URL"] = "https://www.bbc.com/"
-   
     canary.ALERT_TOPIC_ARN = sns_topic
     canary.TARGET_URL = "https://www.bbc.com/"
     yield
@@ -49,13 +59,12 @@ def test_lambda_handler_success(mock_put_metric):
     event = {}
     context = {}
 
-    with mock_aws():
+    with mock_sns():
         response = canary.lambda_handler(event, context)
 
     assert response["page_loaded"] == 1
     assert "latency" in response
     assert "time_to_interactive" in response
-
     mock_put_metric.assert_called_once()
 
 
@@ -63,9 +72,9 @@ def test_lambda_handler_success(mock_put_metric):
 @patch("canary.sns.publish")
 def test_lambda_handler_failure_triggers_alert(mock_sns_publish, mock_put_metric):
     def fail_load(url, timeout=10):
-        return 0, 0, 0
+        return 0, 0, 0  
 
-    with mock_aws():
+    with mock_sns():
         with patch("canary.check_page_load", side_effect=fail_load):
             with pytest.raises(Exception) as exc_info:
                 canary.lambda_handler({}, {})
@@ -73,6 +82,7 @@ def test_lambda_handler_failure_triggers_alert(mock_sns_publish, mock_put_metric
     assert "Page load failed" in str(exc_info.value)
     mock_put_metric.assert_called_once()
     mock_sns_publish.assert_called_once()
+
     args, kwargs = mock_sns_publish.call_args
     assert kwargs["TopicArn"] == canary.ALERT_TOPIC_ARN
     assert "could NOT be loaded" in kwargs["Message"]
--- lambda_22128867/canary/test_canary_unit.py
+++ lambda_22128867/canary/test_canary_unit.py
@@ -1,6 +1,21 @@
+import sys
+import types
 import pytest
 from unittest.mock import patch, MagicMock
 from botocore.exceptions import ClientError
+
+
+if sys.platform == "win32":
+    fake_resource = types.SimpleNamespace()
+    fake_resource.RUSAGE_SELF = 0
+
+    def fake_getrusage(who):
+        return types.SimpleNamespace(ru_maxrss=123456)
+
+    fake_resource.getrusage = fake_getrusage
+    sys.modules['resource'] = fake_resource
+
+
 import canary
 
 TEST_URLS = [
@@ -33,7 +48,8 @@ def test_check_page_load_failure(mock_urlopen, url):
 
 @patch("canary.cloudwatch.put_metric_data")
 def test_put_metrics_success(mock_put):
-    canary.put_metrics(TEST_URLS[0], 0.5, 1, 0.3)
+
+    canary.put_metrics(TEST_URLS[0], 0.5, 1, 0.2, 123.4, 0.3)
     mock_put.assert_called_once()
 
 
@@ -42,8 +58,8 @@ def test_put_metrics_success(mock_put):
     operation_name='PutMetricData'
 ))
 def test_put_metrics_failure(mock_put):
-   
-    canary.put_metrics(TEST_URLS[1], 0.5, 1, 0.3)
+  
+    canary.put_metrics(TEST_URLS[1], 0.5, 1, 0.2, 123.4, 0.3)
 
 
 @patch("canary.sns.publish")
@@ -57,6 +73,4 @@ def test_send_alert_success(mock_publish):
     operation_name='Publish'
 ))
 def test_send_alert_failure(mock_publish):
-   
     canary.send_alert("arn:aws:sns:ap-southeast-2:123456789012:test", TEST_URLS[2])
-
